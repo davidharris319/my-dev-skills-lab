a144262eaa5c91c9895b4f070738b29a31a8db1f
David Harris <davidharris319@gmail.com>
2020-09-20 13:00:55 -0400

    Completed lab work

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+// Variable for input button
+inputButton = $('.input');
+
+//Add skills
+$('#addSkill').on('click', function(e) {
+    
+    const newSkill = inputButton[0].value;
+    const addedSkill = $(`
+    <tr>    
+        <td><button>X</button></td>
+        <td>${newSkill}</td>
+    </tr>
+    `);
+
+    $('table').append(addedSkill);
+    inputButton[0].value = "";
+});
+
+
+// Remove Skills
+$('td').on('click', 'button', function() {
+    $(this).closest('tr').fadeOut(1000, function() {
+        $(this).remove();
+    });
+});
